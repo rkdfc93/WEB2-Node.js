1c06aa0cf431918f3fb15cc3bf777a6dc045cb53
Yujin <rkdfc93@gmail.com>
2021-04-09 18:52:40 +0900

    ~class 27

--- syntax/conditional.js
+++ syntax/conditional.js
@@ -1,8 +1,10 @@
+var args = process.argv;
+console.log(args[2]);
 console.log('A');
 console.log('B');
-if(false){
+if(args[2] === '1'){
   console.log('C1');
-}else{
+} else {
   console.log('C2');
 }
 console.log('D');
